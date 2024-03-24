ebde666ec2fdf54b96c52017f724f3c3627f1869
aliagasiyev <agasiyevali2@gmail.com>
2024-03-24 11:16:39 +0400

    Lesson03 attached

---
+++ src/main/java/az/edu/turing/module02/lesson03/CircleApp.java
@@ -0,0 +1,10 @@
+package az.edu.turing.module02.lesson03;
+
+public class CircleApp {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(5);
+        Circle circle3 = new Circle(52);
+        Circle circle2 = new Circle(15);
+    }
+}
+
---
+++ src/main/java/az/edu/turing/module02/lesson03/PersonApp.java
@@ -0,0 +1,13 @@
+package az.edu.turing.module02.lesson03;
+
+import az.edu.turing.module02.lesson02.Student;
+import az.edu.turing.module02.lesson02.Teacher;
+
+public class PersonApp {
+    public static void main(String[] args) {
+        Student student=new Student("Ali",19);
+        Teacher teacher=new Teacher("Elvin",122222,9);
+
+
+    }
+}
