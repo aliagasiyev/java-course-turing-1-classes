bee2f5a01127381777289cb6da70bc596b5375c5
aliagasiyev <agasiyevali2@gmail.com>
2024-03-24 11:12:35 +0400

    Lesson01 attached

---
+++ src/main/java/az/edu/turing/module02/lesson01/Student.java
@@ -0,0 +1,13 @@
+package az.edu.turing.module02.lesson01;
+
+public class Student {
+    public String name;
+    public int age;
+    public double grade;
+
+    public Student(String name, int age, double grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
